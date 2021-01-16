57c41ebdcd3af3814b0cd0f8f3c0875eb635ccb6
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-16 13:09:44 -0500

    Create Player, Team, and some tests in PlayerTest

---
+++ src/test/java/org/example/PlayerTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.example.exceptions.MissedShotException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void shootsAndScores() {
+        Player player = new Player();
+        player.setShootingAverage(100.0);
+
+        boolean expected = true;
+        boolean actual = false;
+        try {
+            player.shoot();
+            actual = true;
+        } catch (MissedShotException exception) {
+            actual = false;
+        }
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shootsAndMisses() {
+        Player player = new Player();
+        player.setShootingAverage(0.0);
+
+        boolean expected = false;
+        boolean actual = false;
+        try {
+            player.shoot();
+            actual = true;
+        } catch (MissedShotException exception) {
+            actual = false;
+        }
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
